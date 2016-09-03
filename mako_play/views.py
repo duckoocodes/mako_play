9016fae88961a5e919325daa520f5f46c106ffa0
duckoocodes <emgienger@gmail.com>
2016-09-02 18:22:08 -0700

    Added lesson1 route and template. Modified views.py to include lesson1. Ignored sublime cruft in gitignore

--- mako_play/views.py
+++ mako_play/views.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.mako')
 def my_view(request):
     return {'project': 'mako_play'}
+
+@view_config(route_name='lesson1',renderer='templates/lesson1.mako')
+def lesson1(request):
+    return {'my_variable': 'this is my very first mako'}
