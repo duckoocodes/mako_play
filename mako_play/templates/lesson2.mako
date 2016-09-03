51a4d07d1e5f8f8b74006060af76a741228c190a
duckoocodes <emgienger@gmail.com>
2016-09-03 13:47:20 -0700

    Added lesson 2 for filtering concepts. Development.ini set the default filters to nothing.

---
+++ mako_play/templates/lesson2.mako
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        ${show_this_text | h,trim}
+    </body>
+</html>
--- mako_play/views.py
+++ mako_play/views.py
@@ -8,3 +8,12 @@ def my_view(request):
 @view_config(route_name='lesson1',renderer='templates/lesson1.mako')
 def lesson1(request):
     return {'my_variable': 'this is my very first mako'}
+
+@view_config(route_name='lesson2',renderer='templates/lesson2.mako')
+def lesson2(request):
+
+    # hey folks, I swear my scraper only returns text so you do not need to
+    # escape it!!
+    scrapped_text = "<a href='http://google.com/'>hello</a>"
+
+    return {"show_this_text": scrapped_text}
